d5cb2bcb206977c2f4cf6677dc4993c8f908a306
djamper103 <xodorenko95@gmail.com>
2021-06-13 11:58:39 +0300

    first commit

--- src/Components/MainPage/Content/NewReleases/NewReleases.jsx
+++ src/Components/MainPage/Content/NewReleases/NewReleases.jsx
@@ -5,21 +5,50 @@ import style from "./NewReleases.module.css";
 
 
 const  NewReleases=({state,addToCart,})=> {
-
+    const[typeItem,setTypeItem]=useState("all")
+    const[priceItem,setPriceItem]=useState("all")
+    
 
 
     return (
         <div className={style.main}>
             <h3>New Releases</h3>
+            <select name="select" onChange={event=>{setTypeItem(event.target.value)}}>
+                <option value="all" selected>All</option>
+                <option value="shoes" >Shoes</option>
+                <option value="pants">Pants</option>
+                <option value="polo" >Polo</option>
+                <option value="bag">Bag</option>
+            </select>
+            <select name="select" onChange={event=>{setPriceItem(event.target.value)}}>
+                <option value="all" selected>All</option>
+                <option value="mostPrise" >Most Prise</option>
+                <option value="lowPrise">Low Prise</option>
+            </select>
             <div className={style.sale}>
-            {state.map((product) => (
+            {   state.filter(product=>{
+                if(priceItem==="mostPrise"){
+                    return state.sort((a,b)=>b.price-a.price)
+                }else if (priceItem==="lowPrise"){
+                    return state.sort((a,b)=>a.price-b.price)
+                }else if (priceItem==="all"){
+                    return product
+                }
+            })
+                .filter(product=>{
+                if(product.type===typeItem){
+                    return product
+                }else if (typeItem==="all"){
+                    return state
+                }
+                }).map((product) => (
                     <div className={style.component} key={product.id}>
                         <NavLink to={`/Product/${product.id}`}>
                     <img src={product.image} alt={product.id} title={product.id}/>
                         <div className={style.product}>{product.id}</div>
                         </NavLink>
                         <div className={style.size}>{product.size}</div>
-                        <div className={style.price}>{product.salePrice} грн.<p>{product.price} грн.</p></div>
+                        <div className={style.price}><p>{product.price} грн.</p></div>
                     <button onClick={()=>addToCart(product)}>Add to Cart</button>
                     </div>
                     ))
