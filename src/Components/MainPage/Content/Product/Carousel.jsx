1ccfa30777ad134e748b873de392963cf0e51daa
djamper103 <xodorenko95@gmail.com>
2021-07-12 20:45:12 +0300

    first commit

---
+++ src/Components/MainPage/Content/Product/Carousel.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import style from './Carousel.module.css'
+
+
+export default function CarouselBoxProduct({image1,image2,image3}) {
+
+
+    return (
+        <div className={style.transition}>
+            <Carousel>
+                <Carousel.Item>
+                <img src={image1} alt={""} />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                <img src={image2} alt={""} />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                <img src={image3} alt={""} />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    )
+
+}
